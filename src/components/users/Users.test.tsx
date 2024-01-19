9c3b970f2db477c81f25ac1a25ab286b10c443f6
Shin-sibainu <shincode0712@gmail.com>
2024-01-19 19:58:54 +0900

    mod

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { Users } from './Users'
+import { rest } from 'msw'
+import { server } from '../../mocks/server'
 
 describe('Users', () => {
   test('renders correctly', () => {
@@ -12,5 +14,19 @@ describe('Users', () => {
     render(<Users />)
     const users = await screen.findAllByRole('listitem')
     expect(users).toHaveLength(2)
+  })
+
+  test('renders error', async () => {
+    server.use(
+      rest.get(
+        'https://jsonplaceholder.typicode.com/users',
+        (req, res, ctx) => {
+          return res(ctx.status(500))
+        }
+      )
+    )
+    render(<Users />)
+    const error = await screen.findByText('Error Fetching users')
+    expect(error).toBeInTheDocument()
   })
 })
--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -7,10 +7,12 @@ export const Users = () => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
       .then((data) => setUsers(data.map((user: { name: string }) => user.name)))
+      .catch(() => setError('Error Fetching users'))
   }, [])
   return (
     <div>
       <h1>Users</h1>
+      {error && <p>{error}</p>}
       <ul>
         {users.map((user) => (
           <li key={user}>{user}</li>
