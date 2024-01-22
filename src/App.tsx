f60169c966ce1f9b7ecb6bceb48a26097ea801d1
Shin-sibainu <shincode0712@gmail.com>
2024-01-22 11:10:08 +0900

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import Contact from './components/contact/Contact'
 import { Counter } from './components/counter/Counter'
 import { Users } from './components/users/Users'
-
 export default function App() {
   return (
     <div className="App">
--- src/components/contact/Contact.test.tsx
+++ src/components/contact/Contact.test.tsx
@@ -1,62 +1,60 @@
-import { render, screen } from "@testing-library/react";
-import Contact from "./Contact";
+import { render, screen } from '@testing-library/react'
+import Contact from './Contact'
+describe('Contact', () => {
+  test('renders correctly', () => {
+    render(<Contact />)
+    const h1Text = screen.getByRole('heading', {
+      name: 'お問い合わせフォーム',
+    })
+    expect(h1Text).toBeInTheDocument()
 
-describe("Contact", () => {
-  test("renders correctly", () => {
-    render(<Contact />);
+    const h2Text = screen.getByRole('heading', {
+      name: 'H2タグ',
+    })
+    expect(h2Text).toBeInTheDocument()
 
-    const h1Text = screen.getByRole("heading", {
-      name: "お問い合わせフォーム",
-    });
-    expect(h1Text).toBeInTheDocument();
+    const closeElement = screen.getByTitle('close')
+    expect(closeElement).toBeInTheDocument()
 
-    const h2Text = screen.getByRole("heading", {
-      name: "H2タグ",
-    });
-    expect(h2Text).toBeInTheDocument();
+    const paragraphText = screen.getByText('すべてのフィールドは必須項目です。')
+    expect(paragraphText).toBeInTheDocument()
 
-    const closeElement = screen.getByTitle("close");
-    expect(closeElement).toBeInTheDocument();
+    const imageElement = screen.getByAltText('sample alt')
+    expect(imageElement).toBeInTheDocument()
 
-    const paragraphText =
-      screen.getByText("すべてのフィールドは必須項目です。");
-    expect(paragraphText).toBeInTheDocument();
+    const imageElement2 = screen.getByRole('img')
+    expect(imageElement2).toBeInTheDocument()
 
-    const imageElement = screen.getByAltText("sample alt");
-    expect(imageElement).toBeInTheDocument();
+    const customElemet = screen.getByTestId('custom-element')
+    expect(customElemet).toBeInTheDocument()
 
-    const imageElement2 = screen.getByRole("img");
-    expect(imageElement2).toBeInTheDocument();
-
-    const customElemet = screen.getByTestId("custom-element");
-    expect(customElemet).toBeInTheDocument();
-
-    const nameElement = screen.getByRole("textbox", {
-      name: "お名前",
-    });
-    expect(nameElement).toBeInTheDocument();
+    const nameElement = screen.getByRole('textbox', {
+      name: 'お名前',
+    })
+    expect(nameElement).toBeInTheDocument()
 
     // const nameElement2 = screen.getByLabelText("お名前");
     // expect(nameElement2).toBeInTheDocument();
 
-    const nameElement3 = screen.getByPlaceholderText("フルネーム");
-    expect(nameElement3).toBeInTheDocument();
+    const nameElement3 = screen.getByPlaceholderText('フルネーム')
+    expect(nameElement3).toBeInTheDocument()
 
-    const nameElement4 = screen.getByDisplayValue("ShinCode");
-    expect(nameElement4).toBeInTheDocument();
+    const nameElement4 = screen.getByDisplayValue('ShinCode')
+    expect(nameElement4).toBeInTheDocument()
 
-    const emailElement = screen.getByRole("textbox", {
-      name: "メールアドレス",
-    });
-    expect(emailElement).toBeInTheDocument();
+    const emailElement = screen.getByRole('textbox', {
+      name: 'メールアドレス',
+    })
+    expect(emailElement).toBeInTheDocument()
 
-    const questionsElement = screen.getByRole("combobox");
-    expect(questionsElement).toBeInTheDocument();
+    const questionsElement = screen.getByRole('combobox')
+    expect(questionsElement).toBeInTheDocument()
 
-    const termsElement = screen.getByRole("checkbox");
-    expect(termsElement).toBeInTheDocument();
+    const termsElement = screen.getByRole('checkbox')
+    expect(termsElement).toBeInTheDocument()
 
-    const submitButtonElement = screen.getByRole("button");
-    expect(submitButtonElement).toBeInTheDocument();
-  });
-});
+    const submitButtonElement = screen.getByRole('button')
+    expect(submitButtonElement).toBeInTheDocument()
+    expect(submitButtonElement).toBeEnabled()
+  })
+})
